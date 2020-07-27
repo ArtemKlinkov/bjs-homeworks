deaf5889728ceadb9216dece6e1ac13484f109b7
Artem <artemklinkov@yandex.ru>
2020-07-27 20:34:45 +0300

    Решение первого домашнего задания

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,29 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+	// реализуйте здесь задание №1
+	let algebra = a;
+	let geography = g;
+	let physics = p;
     
-	return // ваш ответ;
+	return (algebra + geography + physics) /3;
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
+	// реализуйте здесь задание №2
+	let myName = userName;
     
-	return // ваш ответ;
+	return `Привет, мир! Меня зовут ${myName}`;
 }
 
 function calculateFormula() {
     
 	// реализуйте здесь задание №3
+	let x = 2;
+	let y = 22;
+	let z = 0;
+	let result = x * y + 5 * z + x - 1;
     
-    return // ваш ответ;
+    return result;
 }
