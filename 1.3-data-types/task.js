5d69b0da5f1d01e6ad2f2e12afa0c2bb26c2daee
Artem <artemklinkov@yandex.ru>
2020-07-31 21:18:05 +0300

    Home work 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,42 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    let totalAmount = 0;
+    let param = '';
+    let value = '';
+
+    let str = `Параметр ${param} содержит неправильное значение ${value}`;
+    
+    if (!Number(percent)) {
+        param = 'percent';
+        value = percent;
+        console.log(str);
+    }
+    
+    if (!Number(contribution)) {
+        param = 'contribution';
+        value = contribution;
+        console.log(str);
+    }
+
+    if (!Number(amount)) {
+        param = 'amount';
+        value = amount;
+        console.log(str);
+    }    
+
+    let S = amount - contribution;
+    let P = (percent / 100) / 12;
+    let n = (date.getFullYear() - new Date().getFullYear()) * 12;
+    
+    totalAmount = parseFloat(((S * (P + P / (Math.pow(1 + P, n) - 1))) * n).toFixed(2));
+
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+
+    return `Привет, мир! Меня зовут ${name ? name : 'Аноним'}`;
+    
 }
