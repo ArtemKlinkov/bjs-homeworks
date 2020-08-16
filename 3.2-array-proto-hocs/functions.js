30d7bb6780a5a538423a74ef2b42cef9cd18a9ca
Artem <artemklinkov@yandex.ru>
2020-08-16 10:55:28 +0300

    Homework 3.2

---
+++ 3.2-array-proto-hocs/functions.js
@@ -0,0 +1,21 @@
+const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
+
+function getNames() {
+    return weapons.map(weapon => weapon.name);
+}
+
+function getCountReliableWeapons(durability) {
+    return weapons.filter(weapon => weapon.durability > durability).length;
+}
+
+function hasReliableWeapons(durability) {
+    return weapons.some(weapon => weapon.durability > durability)
+}
+
+function getReliableWeaponsNames(durability) {
+    return weapons.filter(weapon => weapon.durability > durability).map(weapon => weapon.name);
+}
+
+function getTotalDamage() {
+    return weapons.reduce((sumAttack, weapon) => sumAttack + weapon.attack, 0);
+}
